90a0cdac726e0ab50ea8a7d3e2741233f4876b15
RageJello <RageJello@gmail.com>
2018-09-16 20:19:45 -0600

    Initial commit

---
+++ Weekly Time Sheet/Weekly Time Sheet/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Weekly_Time_Sheet
+{
+    partial class weeklyTimeSheet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(weeklyTimeSheet));
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.streetAddressTextBox = new System.Windows.Forms.TextBox();
+            this.streetAddressLabel = new System.Windows.Forms.Label();
+            this.zipCodeTextBox = new System.Windows.Forms.TextBox();
+            this.zipCodeLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.eMailLabel = new System.Windows.Forms.Label();
+            this.eMailTextBox = new System.Windows.Forms.TextBox();
+            this.hourlyRateLabel = new System.Windows.Forms.Label();
+            this.hourlyRateTextBox = new System.Windows.Forms.TextBox();
+            this.hoursWorkedLabel = new System.Windows.Forms.Label();
+            this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.grossPayLabel = new System.Windows.Forms.Label();
+            this.grossPayControlLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(56, 86);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(116, 25);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            this.firstNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(235, 79);
+            this.firstNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(279, 31);
+            this.firstNameTextBox.TabIndex = 1;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(716, 79);
+            this.lastNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(285, 31);
+            this.lastNameTextBox.TabIndex = 3;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(577, 86);
+            this.lastNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(115, 25);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // streetAddressTextBox
+            // 
+            this.streetAddressTextBox.Location = new System.Drawing.Point(235, 155);
+            this.streetAddressTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.streetAddressTextBox.Name = "streetAddressTextBox";
+            this.streetAddressTextBox.Size = new System.Drawing.Size(457, 31);
+            this.streetAddressTextBox.TabIndex = 5;
+            // 
+            // streetAddressLabel
+            // 
+            this.streetAddressLabel.AutoSize = true;
+            this.streetAddressLabel.Location = new System.Drawing.Point(56, 162);
+            this.streetAddressLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.streetAddressLabel.Name = "streetAddressLabel";
+            this.streetAddressLabel.Size = new System.Drawing.Size(154, 25);
+            this.streetAddressLabel.TabIndex = 4;
+            this.streetAddressLabel.Text = "Street Address";
+            // 
+            // zipCodeTextBox
+            // 
+            this.zipCodeTextBox.Location = new System.Drawing.Point(879, 155);
+            this.zipCodeTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zipCodeTextBox.Name = "zipCodeTextBox";
+            this.zipCodeTextBox.Size = new System.Drawing.Size(123, 31);
+            this.zipCodeTextBox.TabIndex = 7;
+            // 
+            // zipCodeLabel
+            // 
+            this.zipCodeLabel.AutoSize = true;
+            this.zipCodeLabel.Location = new System.Drawing.Point(733, 162);
+            this.zipCodeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.zipCodeLabel.Name = "zipCodeLabel";
+            this.zipCodeLabel.Size = new System.Drawing.Size(99, 25);
+            this.zipCodeLabel.TabIndex = 6;
+            this.zipCodeLabel.Text = "Zip Code";
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Location = new System.Drawing.Point(56, 238);
+            this.phoneNumberLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(161, 25);
+            this.phoneNumberLabel.TabIndex = 8;
+            this.phoneNumberLabel.Text = "Phone Number ";
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(235, 234);
+            this.phoneNumberTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(209, 31);
+            this.phoneNumberTextBox.TabIndex = 9;
+            // 
+            // eMailLabel
+            // 
+            this.eMailLabel.AutoSize = true;
+            this.eMailLabel.Location = new System.Drawing.Point(577, 245);
+            this.eMailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.eMailLabel.Name = "eMailLabel";
+            this.eMailLabel.Size = new System.Drawing.Size(73, 25);
+            this.eMailLabel.TabIndex = 10;
+            this.eMailLabel.Text = "E-Mail";
+            // 
+            // eMailTextBox
+            // 
+            this.eMailTextBox.Location = new System.Drawing.Point(716, 238);
+            this.eMailTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.eMailTextBox.Name = "eMailTextBox";
+            this.eMailTextBox.Size = new System.Drawing.Size(285, 31);
+            this.eMailTextBox.TabIndex = 11;
+            this.eMailTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // hourlyRateLabel
+            // 
+            this.hourlyRateLabel.AutoSize = true;
+            this.hourlyRateLabel.Location = new System.Drawing.Point(95, 342);
+            this.hourlyRateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.hourlyRateLabel.Name = "hourlyRateLabel";
+            this.hourlyRateLabel.Size = new System.Drawing.Size(125, 25);
+            this.hourlyRateLabel.TabIndex = 12;
+            this.hourlyRateLabel.Text = "Hourly Rate";
+            this.hourlyRateLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // hourlyRateTextBox
+            // 
+            this.hourlyRateTextBox.Location = new System.Drawing.Point(268, 335);
+            this.hourlyRateTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.hourlyRateTextBox.Name = "hourlyRateTextBox";
+            this.hourlyRateTextBox.Size = new System.Drawing.Size(132, 31);
+            this.hourlyRateTextBox.TabIndex = 13;
+            // 
+            // hoursWorkedLabel
+            // 
+            this.hoursWorkedLabel.AutoSize = true;
+            this.hoursWorkedLabel.Location = new System.Drawing.Point(621, 341);
+            this.hoursWorkedLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.hoursWorkedLabel.Name = "hoursWorkedLabel";
+            this.hoursWorkedLabel.Size = new System.Drawing.Size(149, 25);
+            this.hoursWorkedLabel.TabIndex = 14;
+            this.hoursWorkedLabel.Text = "Hours Worked";
+            // 
+            // hoursWorkedTextBox
+            // 
+            this.hoursWorkedTextBox.Location = new System.Drawing.Point(793, 334);
+            this.hoursWorkedTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+            this.hoursWorkedTextBox.Size = new System.Drawing.Size(132, 31);
+            this.hoursWorkedTextBox.TabIndex = 15;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(376, 434);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(341, 48);
+            this.calculateButton.TabIndex = 16;
+            this.calculateButton.Text = "Calculate Gross Pay";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // grossPayLabel
+            // 
+            this.grossPayLabel.AutoSize = true;
+            this.grossPayLabel.Location = new System.Drawing.Point(371, 542);
+            this.grossPayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.grossPayLabel.Name = "grossPayLabel";
+            this.grossPayLabel.Size = new System.Drawing.Size(112, 25);
+            this.grossPayLabel.TabIndex = 17;
+            this.grossPayLabel.Text = "Gross Pay";
+            // 
+            // grossPayControlLabel
+            // 
+            this.grossPayControlLabel.AutoSize = true;
+            this.grossPayControlLabel.Location = new System.Drawing.Point(582, 542);
+            this.grossPayControlLabel.Name = "grossPayControlLabel";
+            this.grossPayControlLabel.Size = new System.Drawing.Size(0, 25);
+            this.grossPayControlLabel.TabIndex = 18;
+            // 
+            // weeklyTimeSheet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 638);
+            this.Controls.Add(this.grossPayControlLabel);
+            this.Controls.Add(this.grossPayLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.hoursWorkedTextBox);
+            this.Controls.Add(this.hoursWorkedLabel);
+            this.Controls.Add(this.hourlyRateTextBox);
+            this.Controls.Add(this.hourlyRateLabel);
+            this.Controls.Add(this.eMailTextBox);
+            this.Controls.Add(this.eMailLabel);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.zipCodeTextBox);
+            this.Controls.Add(this.zipCodeLabel);
+            this.Controls.Add(this.streetAddressTextBox);
+            this.Controls.Add(this.streetAddressLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "weeklyTimeSheet";
+            this.Text = "Weekly Time Sheet";
+            this.Load += new System.EventHandler(this.weeklyTimeSheet_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox streetAddressTextBox;
+        private System.Windows.Forms.Label streetAddressLabel;
+        private System.Windows.Forms.TextBox zipCodeTextBox;
+        private System.Windows.Forms.Label zipCodeLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.Label eMailLabel;
+        private System.Windows.Forms.TextBox eMailTextBox;
+        private System.Windows.Forms.Label hourlyRateLabel;
+        private System.Windows.Forms.TextBox hourlyRateTextBox;
+        private System.Windows.Forms.Label hoursWorkedLabel;
+        private System.Windows.Forms.TextBox hoursWorkedTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label grossPayLabel;
+        private System.Windows.Forms.Label grossPayControlLabel;
+    }
+}
+
---
+++ Weekly Time Sheet/Weekly Time Sheet/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weekly_Time_Sheet
+{
+    public partial class weeklyTimeSheet : Form
+    {
+        public weeklyTimeSheet()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Declare Variables
+            double hoursWorked;
+            double hourlyRate;
+            double grossPay;
+
+            //Get the hours worked from the TextBox
+            hoursWorked = double.Parse(hoursWorkedTextBox.Text);
+            hourlyRate = double.Parse(hourlyRateTextBox.Text);
+            grossPay = hoursWorked * hourlyRate;
+            grossPayControlLabel.Text = grossPay.ToString();
+
+        }
+
+        private void weeklyTimeSheet_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
