980f9a8ea5d7153813d5f3c8853a9b41ed39adca
RageJello <jspino3141@gmail.com>
2018-11-05 20:19:48 -0800

    Add files via upload

---
+++ FootballStandings/FootballStandings/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FootballStandings
+{
+    partial class football
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(football));
+            this.afcEastButton = new System.Windows.Forms.Button();
+            this.afcSouthButton = new System.Windows.Forms.Button();
+            this.nfcNorthButton = new System.Windows.Forms.Button();
+            this.afcWestButton = new System.Windows.Forms.Button();
+            this.nfcEastButton = new System.Windows.Forms.Button();
+            this.nfcSouthButton = new System.Windows.Forms.Button();
+            this.nfcWestButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.teamsTextBox = new System.Windows.Forms.TextBox();
+            this.afcNorthButton = new System.Windows.Forms.Button();
+            this.saveStandingsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // afcEastButton
+            // 
+            this.afcEastButton.Location = new System.Drawing.Point(26, 96);
+            this.afcEastButton.Name = "afcEastButton";
+            this.afcEastButton.Size = new System.Drawing.Size(194, 48);
+            this.afcEastButton.TabIndex = 0;
+            this.afcEastButton.Text = "AFC East";
+            this.afcEastButton.UseVisualStyleBackColor = true;
+            this.afcEastButton.Click += new System.EventHandler(this.afcEastButton_Click);
+            // 
+            // afcSouthButton
+            // 
+            this.afcSouthButton.Location = new System.Drawing.Point(26, 252);
+            this.afcSouthButton.Name = "afcSouthButton";
+            this.afcSouthButton.Size = new System.Drawing.Size(194, 48);
+            this.afcSouthButton.TabIndex = 1;
+            this.afcSouthButton.Text = "AFC South";
+            this.afcSouthButton.UseVisualStyleBackColor = true;
+            this.afcSouthButton.Click += new System.EventHandler(this.afcSouthButton_Click);
+            // 
+            // nfcNorthButton
+            // 
+            this.nfcNorthButton.Location = new System.Drawing.Point(290, 24);
+            this.nfcNorthButton.Name = "nfcNorthButton";
+            this.nfcNorthButton.Size = new System.Drawing.Size(194, 48);
+            this.nfcNorthButton.TabIndex = 3;
+            this.nfcNorthButton.Text = "NFC North";
+            this.nfcNorthButton.UseVisualStyleBackColor = true;
+            this.nfcNorthButton.Click += new System.EventHandler(this.nfcNorthButton_Click);
+            // 
+            // afcWestButton
+            // 
+            this.afcWestButton.Location = new System.Drawing.Point(26, 169);
+            this.afcWestButton.Name = "afcWestButton";
+            this.afcWestButton.Size = new System.Drawing.Size(194, 48);
+            this.afcWestButton.TabIndex = 2;
+            this.afcWestButton.Text = "AFC West";
+            this.afcWestButton.UseVisualStyleBackColor = true;
+            this.afcWestButton.Click += new System.EventHandler(this.afcWestButton_Click);
+            // 
+            // nfcEastButton
+            // 
+            this.nfcEastButton.Location = new System.Drawing.Point(290, 96);
+            this.nfcEastButton.Name = "nfcEastButton";
+            this.nfcEastButton.Size = new System.Drawing.Size(194, 48);
+            this.nfcEastButton.TabIndex = 5;
+            this.nfcEastButton.Text = "NFC East";
+            this.nfcEastButton.UseVisualStyleBackColor = true;
+            this.nfcEastButton.Click += new System.EventHandler(this.nfcEastButton_Click);
+            // 
+            // nfcSouthButton
+            // 
+            this.nfcSouthButton.Location = new System.Drawing.Point(290, 252);
+            this.nfcSouthButton.Name = "nfcSouthButton";
+            this.nfcSouthButton.Size = new System.Drawing.Size(194, 48);
+            this.nfcSouthButton.TabIndex = 7;
+            this.nfcSouthButton.Text = "NFC South";
+            this.nfcSouthButton.UseVisualStyleBackColor = true;
+            this.nfcSouthButton.Click += new System.EventHandler(this.nfcSouthButton_Click);
+            // 
+            // nfcWestButton
+            // 
+            this.nfcWestButton.Location = new System.Drawing.Point(290, 169);
+            this.nfcWestButton.Name = "nfcWestButton";
+            this.nfcWestButton.Size = new System.Drawing.Size(194, 48);
+            this.nfcWestButton.TabIndex = 6;
+            this.nfcWestButton.Text = "NFC West";
+            this.nfcWestButton.UseVisualStyleBackColor = true;
+            this.nfcWestButton.Click += new System.EventHandler(this.nfcWestButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(157, 529);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(188, 23);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // teamsTextBox
+            // 
+            this.teamsTextBox.AcceptsReturn = true;
+            this.teamsTextBox.Location = new System.Drawing.Point(26, 333);
+            this.teamsTextBox.Multiline = true;
+            this.teamsTextBox.Name = "teamsTextBox";
+            this.teamsTextBox.Size = new System.Drawing.Size(458, 123);
+            this.teamsTextBox.TabIndex = 12;
+            // 
+            // afcNorthButton
+            // 
+            this.afcNorthButton.Location = new System.Drawing.Point(26, 24);
+            this.afcNorthButton.Name = "afcNorthButton";
+            this.afcNorthButton.Size = new System.Drawing.Size(194, 48);
+            this.afcNorthButton.TabIndex = 14;
+            this.afcNorthButton.Text = "AFC North";
+            this.afcNorthButton.UseVisualStyleBackColor = true;
+            this.afcNorthButton.Click += new System.EventHandler(this.afcNorthButton_Click);
+            // 
+            // saveStandingsButton
+            // 
+            this.saveStandingsButton.Location = new System.Drawing.Point(157, 485);
+            this.saveStandingsButton.Name = "saveStandingsButton";
+            this.saveStandingsButton.Size = new System.Drawing.Size(188, 23);
+            this.saveStandingsButton.TabIndex = 15;
+            this.saveStandingsButton.Text = "Save Standings";
+            this.saveStandingsButton.UseVisualStyleBackColor = true;
+            this.saveStandingsButton.Click += new System.EventHandler(this.saveStandingsButton_Click);
+            // 
+            // football
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 573);
+            this.Controls.Add(this.saveStandingsButton);
+            this.Controls.Add(this.afcNorthButton);
+            this.Controls.Add(this.teamsTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.nfcSouthButton);
+            this.Controls.Add(this.nfcWestButton);
+            this.Controls.Add(this.nfcEastButton);
+            this.Controls.Add(this.nfcNorthButton);
+            this.Controls.Add(this.afcWestButton);
+            this.Controls.Add(this.afcSouthButton);
+            this.Controls.Add(this.afcEastButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "football";
+            this.Text = "Football Standings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button afcEastButton;
+        private System.Windows.Forms.Button afcSouthButton;
+        private System.Windows.Forms.Button nfcNorthButton;
+        private System.Windows.Forms.Button afcWestButton;
+        private System.Windows.Forms.Button nfcEastButton;
+        private System.Windows.Forms.Button nfcSouthButton;
+        private System.Windows.Forms.Button nfcWestButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox teamsTextBox;
+        private System.Windows.Forms.Button afcNorthButton;
+        private System.Windows.Forms.Button saveStandingsButton;
+    }
+}
+
---
+++ FootballStandings/FootballStandings/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FootballStandings
+{
+    public partial class football : Form
+    {
+        public football()
+        {
+            InitializeComponent();
+        }
+
+        string divisionFileName = "";
+        string teamName = "";
+        StreamReader divisionFile;
+        StreamWriter standingsFile;
+
+        private void TeamNameFiller(string divisionFileName)
+        {
+            divisionFile = File.OpenText(divisionFileName);
+            teamsTextBox.Text = "";
+            while (!divisionFile.EndOfStream)
+            {
+                teamName = divisionFile.ReadLine();
+                teamsTextBox.Text += teamName + Environment.NewLine;
+            }
+            divisionFile.Close();
+        }
+
+            private void DivisionStanding(string divisionHolder)
+        {
+            switch (divisionHolder)
+            {
+                case "afcNorth":
+                    divisionFileName = "AFC North.txt";
+                    break;
+                case "afcEast":
+                    divisionFileName = "AFC East.txt";
+                    break;
+                case "afcWest":
+                    divisionFileName = "AFC West.txt";
+                    break;
+                case "afcSouth":
+                    divisionFileName = "AFC South.txt";
+                    break;
+                case "nfcNorth":
+                    divisionFileName = "NFC North.txt";
+                    break;
+                case "nfcEast":
+                    divisionFileName = "NFC East.txt";
+                    break;
+                case "nfcWest":
+                    divisionFileName = "NFC West.txt";
+                    break;
+                case "nfcSouth":
+                    divisionFileName = "NFC South.txt";
+                    break;
+                default:
+                    MessageBox.Show("Error: Something is very wrong. You should probably panic.");
+                    break;
+            }
+        }
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void afcEastButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("afcEast");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void afcNorthButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("afcNorth");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void afcWestButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("afcWest");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void afcSouthButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("afcSouth");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void nfcNorthButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("nfcNorth");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void nfcEastButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("nfcEast");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void nfcWestButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("nfcWest");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void nfcSouthButton_Click(object sender, EventArgs e)
+        {
+            DivisionStanding("nfcSouth");
+            TeamNameFiller(divisionFileName);
+        }
+
+        private void saveStandingsButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveStandings = new SaveFileDialog();
+            saveStandings.Filter = "txt files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (saveStandings.ShowDialog() == DialogResult.OK)
+            {
+                standingsFile = File.CreateText(saveStandings.FileName);
+                standingsFile.WriteLine(teamsTextBox.Text);
+                standingsFile.Close();
+            }
+        }
+    }
+}
+    
